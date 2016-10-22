594469157693d93bcc5ae0eccb3f7b69e77f7a47
David <david_guetl@gmx.at>
2016-10-22 09:08:15 +0200

    Addon: Downloads

---
+++ Sprachpaket-Du/infusions/downloads/locale/German/downloads.php
@@ -0,0 +1,39 @@
+<?php
+
+// Translate powered by PHPFusion-Development.at
+
+// Main
+$locale['download_1000'] = "Downloads";
+$locale['download_1001'] = "Übersicht";
+$locale['download_1003'] = "Kategorien";
+$locale['download_1004'] = "Veröffentlicher";
+$locale['download_1006'] = "Mehr lesen";
+$locale['download_1007'] = "Datei herunterladen";
+$locale['download_1008'] = "Benutzer Bewertungen";
+$locale['download_1009'] = "Screenshot";
+$locale['download_1010'] = "Allgemeine Informationen";
+$locale['download_1011'] = "Version";
+$locale['download_1012'] = "Heruntergeladen";
+$locale['download_1013'] = "Lizenz";
+$locale['download_1014'] = "Betriebssystem";
+$locale['download_1015'] = "Copyright";
+$locale['download_1016'] = "Erstellungsdatum";
+$locale['download_1017'] = "Webseite";
+$locale['download_1018'] = "Webseite besuchen";
+$locale['download_1019'] = "Beschreibung";
+$locale['download_1020'] = "Dateigröße";
+$locale['download_1021'] = "Veröffentlichungsdatum";
+
+// filters
+$locale['download_2001'] = "Am meisten kommentiert";
+$locale['download_2002'] = "Neueste Downloads";
+$locale['download_2003'] = "Am meisten geladen";
+$locale['download_2004'] = "Am besten bewertet";
+
+// errors
+$locale['download_3000'] = "Es wurden bisher noch keine Downloads erstellt.";
+$locale['download_3001'] = "Keine Kategorien angelegt";
+$locale['download_3002'] = "Keine Veröffentlicher verfügbar";
+$locale['download_3003'] = "Diesen Download ansehen und bewerten";
+
+?>
---
+++ Sprachpaket-Du/infusions/downloads/locale/German/downloads_admin.php
@@ -0,0 +1,163 @@
+<?php
+
+// Translate powered by PHPFusion-Development.at
+
+// Downloads titles
+$locale['download_0000'] = "Downloads";
+$locale['download_0001'] = "Downloads";
+$locale['download_0002'] = "Download erstellen";
+$locale['download_0003'] = "Download bearbeiten";
+$locale['download_0004'] = "Alle Downloads";
+$locale['download_0005'] = "Derzeit werden %d Downloads von %d Downloads angezeigt.";
+
+// Filters
+$locale['download_0010'] = "Sortieren nach";
+$locale['download_0011'] = "Kategorie";
+
+// Downloads Category titles
+$locale['download_0020'] = "Alle Download Kategorien";
+$locale['download_0021'] = "Download Kategorie bearbeiten";
+$locale['download_0022'] = "Download Kategorien";
+$locale['download_0023'] = "Download Kategorien Editor";
+
+// Downloads submissions
+$locale['download_0039'] = "Zurück zur Startseite";
+$locale['download_0040'] = "Entschuldigung! Wir akzeptieren derzeit keine neuen Einsendungen von Downloads.";
+$locale['download_0041'] = "Download einsenden"; // 650
+$locale['download_0042'] = "Vielen Dank für die Einsendung des Downloads.";
+$locale['download_0043'] = "Weiteren Download einsenden";
+$locale['download_0044'] = "Du kannst das nachfolgende Formular für die Einsendung eines neuen Downloads benützen. ";
+$locale['download_0044'] .= "Deine Einsendung wird anschließend durch einen Administrator überprüft.<br />";
+$locale['download_0044'] .= "[SITENAME] behält sich das Recht vor, den Download zu erweitern, abzuändern oder gegebenenfalls zu entfernen. ";
+$locale['download_0044'] .= "Bitte beachte, dass der Inhalt des Downloads passend zu dem Thema der Seite ist.";
+$locale['download_0045'] = "Download einsenden";
+$locale['download_0046'] = "Einsendungen";
+$locale['download_0047'] = "Screenshot verpflichtend?";
+$locale['download_0048'] = "Beschreibung verpflichtend?";
+
+// Downloads submissions - admin
+$locale['download_0049'] = "Einsendungen";
+$locale['download_0050'] = "Es sind derzeit keine neuen Downloads eingesendet worden.";
+$locale['download_0051'] = "Es ist/sind derzeit %s verfügbar, welche auf eine Überprüfung wartet/warten.";
+$locale['download_0052'] = "Titel";
+$locale['download_0053'] = "Einsender";
+$locale['download_0054'] = "Datum";
+$locale['download_0055'] = "ID";
+$locale['download_0056'] = "Der nachfolgende Download wurde eingesendet von ";
+$locale['download_0057'] = "Die Einsendung erfolgte ";
+$locale['download_0060'] = "Entfernen";
+$locale['download_0061'] = "Veröffentlichen";
+$locale['download_0062'] = "Die Download Einsendung wurde erfolgreich entfernt.";
+$locale['download_0063'] = "Die Download Einsendung wurde erfolgreich veröffentlicht.";
+
+// Downloads messages
+$locale['download_0100'] = "Der Download wurde erfolgreich erstellt.";
+$locale['download_0101'] = "Der Download wurde erfolgreich aktualisiert.";
+$locale['download_0102'] = "Der Download wurde erfolgreich entfernt.";
+$locale['download_0103'] = "Der Download konnte nicht erstellt werden.";
+$locale['download_0104'] = "Der Dateiname ist ungültig.";
+$locale['download_0105'] = "Die Datei muss kleiner sein als %s.";
+$locale['download_0106'] = "Das Bild muss kleiner sein als %s.";
+$locale['download_0107'] = "Das Bild muss kleiner sein als %s.";
+$locale['download_0108'] = "Die Datei muss einer der folgenden Dateitypen sein: %s.";
+$locale['download_0109'] = "Das Bild muss einer der folgenden Dateitypen sein: %s.";
+$locale['download_0110'] = "Du musst einen Titel angeben.";
+$locale['download_0111'] = "Du musst einen Download Link angeben oder eine Datei hochladen.";
+$locale['download_0112'] = "Du musst eine Kurz Beschreibung angeben.";
+$locale['download_0113'] = "Es ist ein unbekannter Fehler aufgetreten.";
+$locale['download_0114'] = "Du musst einen Screenshot hochladen.";
+$locale['download_0115'] = "Du musst eine Datei hochladen.";
+$locale['download_0116'] = "Du musst einen Download Link angeben.";
+
+// Downloads Category messages
+$locale['download_0150'] = "Die Download Kategorie wurde erfolgreich erstellt.";
+$locale['download_0151'] = "Die Download Kategorie wurde erfolgreich aktualisiert.";
+$locale['download_0152'] = "Die Download Kategorie konnte nicht entfernt werden.";
+$locale['download_0153'] = "Es befinden sich noch Downloads oder Unterkategorien in der Download Kategorie.";
+$locale['download_0154'] = "Die Download Kategorie wurde erfolgreich entfernt.";
+
+// Downloads form
+$locale['download_0200'] = "Titel";
+$locale['download_0201'] = "Download ansehen";
+$locale['download_0202'] = "Kurz Beschreibung";
+$locale['download_0202a'] = "Beschreibung";
+$locale['download_0203'] = "Tags";
+$locale['download_0203a'] = "Drücke nach jedem Tag die Taste Enter.";
+$locale['download_0204'] = "Download Quelle: Bitte wähle, ob du eine Datei hochladen möchtest, oder auf einen externen Download Link verweisen möchtest.";
+$locale['download_0205'] = "Sichtbarkeit";
+$locale['download_0206'] = "Link";
+$locale['download_0207'] = "Kategorie";
+$locale['download_0208'] = "Lizenz";
+$locale['download_0209'] = "Betriebssystem";
+$locale['download_0210'] = "Version";
+$locale['download_0211'] = "Dateigröße";
+$locale['download_0212'] = "Speichern";
+$locale['download_0213'] = "Veröffentlichungsdatum erneuern";
+$locale['download_0214'] = "Datei hochladen";
+$locale['download_0215'] = "Download Link";
+$locale['download_0216'] = "Entfernen";
+$locale['download_0217'] = "Dateigröße automatisch berechnen";
+$locale['download_0218'] = "Maximale Dateigröße: %s / Erlaubte Dateitypen: %s";
+$locale['download_0219'] = "Maximale Dateigröße: %s<br />Erlaubte Dateitypen: %s<br />Maximale Größe: %s x %s Pixel";
+$locale['download_0220'] = "Screenshot";
+$locale['download_0221'] = "Webseite";
+$locale['download_0222'] = "Copyright";
+$locale['download_0223'] = "Kommentare bei diesem Download aktivieren";
+$locale['download_0224'] = "Bewertungen bei diesem Download aktivieren";
+$locale['download_0225'] = "Abbrechen";
+
+// Current Downloads
+$locale['download_0226'] = "Download testen";
+$locale['download_0227'] = "Optionen";
+
+// No Downloads/Downloads Categories Defined
+$locale['download_0249'] = "Es wurden bisher noch keine Download Kategorien erstellt.";
+$locale['download_0250'] = "Es befinden sich noch keine Downloads oder Unterkategorien in dieser Download Kategorie.";
+$locale['download_0251'] = "Es wurden bisher noch keine Download Kategorien erstellt.";
+$locale['download_0252'] = "Du musst mindestens eine Download Kategorie anlegen, bevor du einen Download erstellen kannst.";
+$locale['download_0253'] = "Klicke hier";
+$locale['download_0254'] = " um zu den Download Kategorien zugehen";
+
+// Popup Error/Warning Messages
+$locale['download_0255'] = "Bist du dir sicher, dass du den Download entfernen möchtest?";
+$locale['download_0256'] = "%s sind für alle Einträge deaktiviert.\n\rDiese Einstellungen sind nur wirksam, wenn sie in den globalen Einstellungen aktiviert werden.";
+
+// Downloads Category form
+$locale['download_0300'] = "Titel";
+$locale['download_0301'] = "Beschreibung";
+$locale['download_0302'] = "Sortierung";
+$locale['download_0303'] = "Download ID";
+$locale['download_0304'] = "Download Titel";
+$locale['download_0305'] = "Download Datum";
+$locale['download_0306'] = "Aufwärts (A-Z)";
+$locale['download_0307'] = "Abwärts (Z-A)";
+$locale['download_0308'] = "Kategorie";
+$locale['download_0309'] = "Speichern";
+
+// Current Downloads Categories
+$locale['download_0310'] = "Kategorie";
+$locale['download_0311'] = "Sichtbarkeit";
+$locale['download_0312'] = "Optionen";
+$locale['download_0313'] = "Es wurden bisher noch keine Download Kategorien erstellt.";
+
+// Delete Downloads category
+$locale['download_0350'] = "Bist du dir sicher, dass du die Download Kategorie entfernen möchtest?";
+
+// Error Messages
+$locale['download_0351'] = "Du musst einen Titel angeben.";
+$locale['download_0352'] = "Es existiert bereits eine gleichnamige Kategorie.";
+$locale['604'] = "Breite x Höhe";
+
+// Downloads Settings
+$locale['930'] = "Maximale Dateigröße";
+$locale['931'] = "Maximale Dateigröße in Bytes.";
+$locale['932'] = "Erlaubte Dateitypen";
+$locale['933'] = "Bitte trenne jeden Dateityp durch ein Komma.";
+$locale['934'] = "Screenshot Größe";
+$locale['935'] = "Breite x Höhe";
+$locale['936'] = "Maximale Bildgröße";
+$locale['937'] = "Screenshot Thumb Größe";
+$locale['938'] = "Download Screenshot aktivieren?";
+$locale['939'] = "Download Einträge pro Seite";
+
+?>
