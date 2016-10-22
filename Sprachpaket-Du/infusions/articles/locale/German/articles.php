6ce555a07a895fb932831dc8574e0f69bc3e1501
David <david_guetl@gmx.at>
2016-10-22 09:05:55 +0200

    Addon: Artikel

---
+++ Sprachpaket-Du/infusions/articles/locale/German/articles.php
@@ -0,0 +1,10 @@
+<?php
+
+// Translate powered by PHPFusion-Development.at
+
+$locale['400'] = "Artikel";
+$locale['401'] = "Es wurden bisher noch keine Artikel Kategorien erstellt.";
+$locale['402'] = "NEU";
+$locale['403'] = "Es wurden bisher noch keine Artikel in dieser Kategorie abgelegt.";
+
+?>
---
+++ Sprachpaket-Du/infusions/articles/locale/German/articles_admin.php
@@ -0,0 +1,121 @@
+<?php
+
+// Translate powered by PHPFusion-Development.at
+
+// Article titles
+$locale['articles_0000'] = "Artikel";
+$locale['articles_0001'] = "Artikel";
+$locale['articles_0002'] = "Artikel erstellen";
+$locale['articles_0003'] = "Artikel bearbeiten";
+
+// Category titles
+$locale['articles_0020'] = "Artikel Kategorien";
+$locale['articles_0021'] = "Artikel Kategorie erstellen";
+$locale['articles_0022'] = "Artikel Kategorie bearbeiten";
+$locale['articles_0023'] = "Alle Artikel Einträge";
+$locale['articles_0024'] = "Derzeit werden %d Artikel von %d Artikel angezeigt.";
+$locale['articles_0025'] = "Sortieren nach";
+$locale['articles_0026'] = "Kategorie";
+$locale['articles_0027'] = "Artikel Kategorie Editor";
+
+// Article Settings
+$locale['articles_0030'] = "Einstellungen";
+$locale['articles_0031'] = "Allgemeine Einstellungen für das Artikel System.";
+$locale['articles_0032'] = "Artikel Einträge pro Seite";
+$locale['articles_0033'] = "Artikel Einsendungen erlauben?";
+$locale['articles_0034'] = "Erweitender Artikel verpflichtend?";
+
+// Article Submissions
+$locale['articles_0040'] = "Einsendungen";
+$locale['articles_0041'] = "Betreff";
+$locale['articles_0042'] = "Es sind derzeit keine neuen Artikel eingesendet worden.";
+$locale['articles_0043'] = "Entschuldigung! Wir akzeptieren derzeit keine neuen Einsendungen von Artikel.";
+$locale['articles_0043a'] = "Die Einsendung von Artikel ist deaktiviert. Es existiert derzeit keine Kategorie für die aktuelle Sprache.";
+$locale['articles_0044'] = "Es ist/sind derzeit %s verfügbar, welche auf eine Überprüfung wartet/warten.";
+$locale['articles_0045'] = "Vorschau";
+$locale['articles_0046'] = "Einsender";
+$locale['articles_0047'] = "Datum";
+$locale['articles_0048'] = "ID";
+$locale['articles_0049'] = "Die Artikel Einsendung wurde erfolgreich entfernt.";
+$locale['articles_0050'] = "Die Artikel Einsendung wurde erfolgreich veröffentlicht.";
+$locale['articles_0051'] = "Die Artikel Einsendung wurde erfolgreich als Entwurf gespeichert.";
+$locale['articles_0052'] = "Der nachfolgende Artikel wurde eingesendet von ";
+$locale['articles_0053'] = "Die Einsendung erfolgte ";
+$locale['articles_0060'] = "Artikel Einsenden";
+$locale['articles_0061'] = "Vielen Dank für die Einsendung des Artikels.";
+$locale['articles_0062'] = "Weiteren Artikel einsenden";
+$locale['articles_0063'] = "Du kannst das nachfolgende Formular für die Einsendung eines neuen Artikels benützen. ";
+$locale['articles_0063'] .= "Deine Einsendung wird anschließend durch einen Administrator überprüft.<br />";
+$locale['articles_0063'] .= "[SITENAME] behält sich das Recht vor, den Artikel zu erweitern, abzuändern oder gegebenenfalls zu entfernen. ";
+$locale['articles_0063'] .= "Bitte beachte, dass der Inhalt des Artikels passend zu dem Thema der Seite ist.";
+$locale['articles_0064'] = "Zurück zur Startseite";
+
+// Article messages
+$locale['articles_0100'] = "Der Artikel wurde erfolgreich erstellt.";
+$locale['articles_0101'] = "Der Artikel wurde erfolgreich aktualisiert.";
+$locale['articles_0102'] = "Der Artikel wurde erfolgreich entfernt.";
+
+// Category messages
+$locale['articles_0150'] = "Die Artikel Kategorie wurde erfolgreich erstellt.";
+$locale['articles_0151'] = "Die Artikel Kategorie wurde erfolgreich aktualisiert.";
+$locale['articles_0152'] = "Die Artikel Kategorie konnte nicht entfernt werden.";
+$locale['articles_0153'] = "Es befinden sich noch Artikel oder Unterkategorien in dieser Artikel Kategorie.";
+$locale['articles_0154'] = "Die Artikel Kategorie wurde erfolgreich entfernt.";
+
+// Article form
+$locale['articles_0200'] = "Betreff";
+$locale['articles_0201'] = "Kategorie";
+$locale['articles_0202'] = "Auszug";
+$locale['articles_0203'] = "Artikel";
+$locale['articles_0204'] = "Tags";
+$locale['articles_0204a'] = "Drücke nach jedem Tag die Taste Enter.";
+$locale['articles_0205'] = "Den Artikel als Entwurf speichern.";
+$locale['articles_0206'] = "Automatische Zeilenumbrüche in diesem Artikel aktivieren.";
+$locale['articles_0207'] = "Kommentare in diesem Artikel aktivieren.";
+$locale['articles_0208'] = "Bewertungen in diesem Artikel aktivieren.";
+$locale['articles_0209'] = "Seitenumbruch";
+$locale['articles_0210'] = "[ENTWURF]";
+$locale['articles_0211'] = "Sichtbarkeit";
+$locale['articles_0240'] = "Vorschau";
+$locale['articles_0241'] = "Speichern";
+$locale['articles_0242'] = "Veröffentlichen";
+$locale['articles_0243'] = "Entfernen";
+
+// Popup Error/Warning Messages
+$locale['articles_0250'] = "Du musst einen Betreff angeben.";
+$locale['articles_0251'] = "Bist du dir sicher, dass du den Artikel entfernen möchtest?";
+$locale['articles_0252'] = "Es wurden bisher noch keine Artikel Kategorien erstellt.";
+$locale['articles_0253'] = "Du musst mindestens eine Artikel Kategorie anlegen, bevor du einen Artikel erstellen kannst.";
+$locale['articles_0254'] = "Klick hier";
+$locale['articles_0255'] = " um zu den Artikel Kategorien zugehen";
+$locale['articles_0256'] = "%s sind für alle Einträge deaktiviert.\n\rDiese Einstellungen sind nur wirksam, wenn sie in den globalen Einstellungen aktiviert werden.";
+$locale['articles_0257'] = "Du musst mindestens einen Tag angeben.";
+
+// Category form
+$locale['articles_0300'] = "Titel";
+$locale['articles_0301'] = "Beschreibung";
+$locale['articles_0302'] = "Sortierung";
+$locale['articles_0303'] = "Artikel ID";
+$locale['articles_0304'] = "Artikel Betreff";
+$locale['articles_0305'] = "Artikel Datum";
+$locale['articles_0306'] = "Aufwärts (A-Z)";
+$locale['articles_0307'] = "Abwärts (Z-A)";
+$locale['articles_0308'] = "Kategorie";
+$locale['articles_0309'] = "Speichern";
+
+// Current category options
+$locale['articles_0340'] = "Kategorie";
+$locale['articles_0341'] = "Optionen";
+$locale['edit'] = "Bearbeiten";
+$locale['delete'] = "Entfernen";
+$locale['articles_0342'] = "Es wurden bisher noch keine Artikel Kategorien erstellt.";
+$locale['articles_0343'] = "Es wurden bisher noch keine Artikel erstellt.";
+
+// Delete Article category
+$locale['articles_0350'] = "Bist du dir sicher, dass du die Artikel Kategorie entfernen möchtest?";
+
+// Error Messages
+$locale['articles_0351'] = "Du musst einen Titel angeben.";
+$locale['articles_0352'] = "Es existiert bereits eine gleichnamige Kategorie.";
+
+?>
