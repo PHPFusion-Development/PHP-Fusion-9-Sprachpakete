992e5f9aa13ee846e08a8793a1f7b26438b3062f
David <david_guetl@gmx.at>
2016-10-22 09:10:07 +0200

    Addon: Häufige Fragen

---
+++ Sprachpaket-Du/infusions/faq/locale/German/faq.php
@@ -0,0 +1,14 @@
+<?php
+
+// Translate powered by PHPFusion-Development.at
+
+// Main
+$locale['400'] = "Häufige Fragen";
+$locale['401'] = "Häufige Fragen";
+$locale['402'] = "Nach Oben";
+
+// Notices
+$locale['410'] = "Es wurden bisher noch keine Kategorien angelegt.";
+$locale['411'] = "Es wurden bisher noch keine Einträge in dieser Kategorie abgelegt.";
+
+?>
---
+++ Sprachpaket-Du/infusions/faq/locale/German/faq_admin.php
@@ -0,0 +1,48 @@
+<?php
+
+// Translate powered by PHPFusion-Development.at
+
+// Main
+$locale['faq_0100'] = "Häufige Fragen";
+$locale['faq_0101'] = "Aktuelle Einträge";
+$locale['faq_0102'] = "Kategorien";
+$locale['faq_0102b'] = "Eintrag erstellen";
+$locale['faq_0102c'] = "Eintrag bearbeiten";
+$locale['faq_0102d'] = "Kategorie bearbeiten";
+$locale['faq_0103'] = "Titel";
+$locale['faq_0104'] = "Enthaltene Einträge";
+$locale['faq_0105'] = "ID";
+$locale['faq_0106'] = "Optionen";
+$locale['faq_0107'] = "Bearbeiten";
+$locale['faq_0108'] = "Entfernen";
+$locale['faq_0109'] = "Bist du dir sicher dass du die Kategorie entfernen möchtest?";
+$locale['faq_0110'] = "Frage";
+$locale['faq_0111'] = "Antwort";
+$locale['faq_0112'] = "Bist du dir sicher, dass du den Eintrag entfernen möchtest?";
+$locale['faq_0113'] = "Es wurden bisher noch keine Einträge angelegt.";
+$locale['faq_0114'] = "Es werden %d von %d Einträgen angezeigt.";
+$locale['faq_0115'] = "Es werden %d von %d Kategorien angezeigt.";
+$locale['faq_0116'] = "Es wurden bisher noch keine Kategorien angelegt.";
+
+// Faq Category form
+$locale['faq_0200'] = "Titel";
+$locale['faq_0201'] = "Du musst einen Titel angeben.";
+$locale['faq_0202'] = "Beschreibung";
+$locale['faq_0203'] = "Speichern";
+$locale['faq_0204'] = "Die Kategorie wurde erfolgreich erstellt.";
+$locale['faq_0205'] = "Die Kategorie wurde erfolgreich aktualisiert.";
+$locale['faq_0206'] = "Die Kategorie wurde erfolgreich entfernt.";
+$locale['faq_0207'] = "Die Kategorie konnte nicht entfernt werden, da noch %d Eintrag/Einträge enthalten ist/sind.";
+
+// Faq Form
+$locale['faq_0300'] = "Kategorie";
+$locale['faq_0301'] = "Frage";
+$locale['faq_0302'] = "Antwort";
+$locale['faq_0303'] = "Speichern";
+$locale['faq_0304'] = "Es muss mindestens eine Kategorie angelegt sein, um Einträge erstellen zu können. ";
+$locale['faq_0304'] .= "Bitte klicke [LINK]hier[/LINK] um eine Kategorie zu erstellen.";
+$locale['faq_0305'] = "Der Eintrag wurde erfolgreich erstellt.";
+$locale['faq_0306'] = "Der Eintrag wurde erfolgreich aktualisiert.";
+$locale['faq_0307'] = "Der Eintrag wurde erfolgreich entfernt.";
+
+?>
