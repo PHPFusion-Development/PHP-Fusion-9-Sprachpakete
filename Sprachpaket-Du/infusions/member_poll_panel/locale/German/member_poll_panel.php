4513debabc3edc8b60c3ebbd6c11137613ede0a3
David <david_guetl@gmx.at>
2016-10-23 06:05:20 +0200

    Addon: Umfragen

---
+++ Sprachpaket-Du/infusions/member_poll_panel/locale/German/member_poll_panel.php
@@ -0,0 +1,21 @@
+<?php
+
+// Translate powered by PHPFusion-Development.at
+
+// Main
+$locale['global_130'] = "Umfragen";
+$locale['global_131'] = "Abstimmen";
+$locale['global_132'] = "Bitte melde dich an um abzustimmen.";
+$locale['global_133'] = "Stimme";
+$locale['global_134'] = "Stimmen";
+$locale['global_135'] = "Stimmen: ";
+$locale['global_136'] = "Gestartet: ";
+$locale['global_137'] = "Beendet: ";
+$locale['global_138'] = "Archiv";
+$locale['global_139'] = "Bitte wähle eine Umfrage aus der Liste aus.";
+$locale['global_140'] = "Ansehen";
+$locale['global_141'] = "Umfrage ansehen";
+$locale['global_142'] = "Es wurden bisher noch keine Umfragen erstellt.";
+$locale['global_143'] = "Bewertungen";
+
+?>
---
+++ Sprachpaket-Du/infusions/member_poll_panel/locale/German/member_poll_panel_admin.php
@@ -0,0 +1,38 @@
+<?php
+
+// Translate powered by PHPFusion-Development.at
+
+// Polls titles
+$locale['400'] = "Umfrage erstellen";
+$locale['401'] = "Umfrage bearbeiten";
+$locale['402'] = "Aktuelle Umfragen";
+$locale['403'] = "Vorschau der Umfrage";
+
+// Polls messages
+$locale['410'] = "Die Umfrage wurde erfolgreich erstellt.";
+$locale['411'] = "Die Umfrage wurde erfolgreich aktualisiert.";
+$locale['412'] = "Die Umfrage wurde erfolgreich entfernt.";
+
+// Edit/Delete Polls
+$locale['420'] = "Aktuelle Umfragen";
+$locale['421'] = "Bearbeiten";
+$locale['422'] = "Entfernen";
+
+// Preview Polls
+$locale['430'] = "Abstimmen";
+
+// Polls Form
+$locale['431'] = "Titel";
+$locale['432'] = "Option ";
+$locale['433'] = " Diese Umfrage beenden.";
+$locale['434'] = "Die Umfrage wurde gestartet am ";
+$locale['435'] = "Die Umfrage wurde beendet am ";
+$locale['436'] = "Weitere Option hinzufügen";
+$locale['437'] = "Vorschau";
+$locale['438'] = "Speichern";
+$locale['439'] = "Nicht definiert";
+$locale['439a'] = "Du musst einen Titel angeben.";
+$locale['439b'] = "Du musst mehr als eine Option angeben.";
+$locale['439c'] = "Umfragen";
+
+?>
