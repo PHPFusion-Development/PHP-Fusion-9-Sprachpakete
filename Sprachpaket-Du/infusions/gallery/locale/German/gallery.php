4211133d57fc23d9a1846f39006fde5b7bef8b78
David <david_guetl@gmx.at>
2016-10-23 05:51:14 +0200

    Addon: Fotogalerie
    ./infusions/gallery/locale/Englisch/gallery_admin_dep.php wurde nicht
    übersetzt, da diese Sprachdatei unserer Ansicht nach nicht mehr
    verwendet wird.

---
+++ Sprachpaket-Du/infusions/gallery/locale/German/gallery.php
@@ -0,0 +1,46 @@
+<?php
+
+// Translate powered by PHPFusion-Development.at
+
+// Main
+$locale['400'] = "Fotogalerie";
+$locale['401'] = "Album öffnen";
+$locale['402'] = "Kein Vorschaubild";
+$locale['403'] = "Datum:";
+$locale['404'] = "Erstellt von: ";
+$locale['405'] = "Fotos: ";
+$locale['406'] = "No Gallery Albums defined.";
+$locale['420'] = "Album Informationen";
+$locale['421'] = "Album: ";
+$locale['422'] = "Enthaltene Fotos: ";
+$locale['423'] = "Das letzte Foto wurde hinzugefügt von ";
+$locale['424'] = " am ";
+$locale['425'] = "No photos have been added to this Gallery album";
+$locale['430'] = "Album ansehen";
+$locale['431'] = "Klicke, um zu sehen.";
+$locale['432'] = "Kein Vorschaubild";
+$locale['433'] = "Veröffentlichungsdatum ";
+$locale['434'] = "Hinzugefügt von ";
+$locale['435'] = "Angesehen: ";
+$locale['436'] = "Kommentare: ";
+$locale['436b'] = "Kommentar: ";
+$locale['437'] = "Bewertung: ";
+$locale['438'] = "Keine";
+$locale['450'] = "Foto ansehen";
+$locale['451'] = "Vorheriges Foto";
+$locale['452'] = "Nächstes Foto";
+$locale['453'] = "Klicke, um das Foto in originaler Größe anzusehen";
+$locale['454'] = "Abmessungen: ";
+$locale['455'] = "Pixel";
+$locale['456'] = "Dateigröße: ";
+$locale['457'] = "Angesehen: ";
+$locale['458'] = "Klicke, um zu schließen";
+$locale['459'] = "Erstes Foto";
+$locale['460'] = "Letztes Foto";
+$locale['461'] = "Foto";
+$locale['462'] = "Fotos";
+$locale['463'] = "Veröffentlicher|Veröffentlicher";
+$locale['464'] = "Hinzugefügt am ";
+$locale['465'] = "Galerie";
+
+?>
---
+++ Sprachpaket-Du/infusions/gallery/locale/German/gallery_admin.php
@@ -0,0 +1,198 @@
+<?php
+
+// Translate powered by PHPFusion-Development.at
+
+// Main
+$locale['gallery_0001'] = "Fotogalerie";
+$locale['gallery_0002'] = "Foto hinzufügen";
+$locale['gallery_0003'] = "Foto bearbeiten";
+$locale['gallery_0004'] = "Album erstellen";
+$locale['gallery_0005'] = "Album bearbeiten";
+$locale['gallery_0006'] = "Einstellungen";
+$locale['gallery_0007'] = "Einsendungen";
+$locale['gallery_0009'] = "Foto hochladen";
+$locale['gallery_0010'] = "Fotos hochladen";
+$locale['gallery_0011'] = "Es wurden bisher noch keine Fotoalben erstellt.";
+$locale['gallery_0012'] = "Es wurden bisher noch keine Fotoalben erstellt. Du musst mindestens ein Fotoalbum erstellen, bevor du ein Foto hinzufügen kannst. <a href='%s'>Klicke hier</a> um zu den Fotoalben zu gehen.";
+$locale['gallery_0013'] = "Optionen";
+$locale['gallery_0014'] = "Aufwärts";
+$locale['gallery_0015'] = "Abwärts";
+$locale['gallery_0016'] = "Bearbeiten";
+$locale['gallery_0017'] = "Entfernen";
+$locale['gallery_0018'] = "Derzeit werden %d Alben von %d Alben angezeigt.";
+$locale['gallery_0019'] = "Derzeit werden %d Fotos von %d Fotos angezeigt.";
+$locale['gallery_0020'] = "Letzte Aktualisierung:";
+$locale['gallery_0021'] = "Sichtbarkeit:";
+$locale['gallery_0022'] = "Fotogalerie";
+$locale['gallery_0023'] = "Es befinden sich derzeit %d Alben und %d Fotos in der Galerie. Die letzte Aktualisierung war %s.";
+$locale['gallery_0024'] = "Es wurden bisher noch keine Fotoalben erstellt.";
+
+// Album Form
+$locale['album_0001'] = "Titel";
+$locale['album_0002'] = "Titel des Fotoalbums";
+$locale['album_0003'] = "Beschreibung";
+$locale['album_0004'] = "Beschreibung des Fotoalbums";
+$locale['album_0005'] = "Tags";
+$locale['album_0006'] = "Drücke nach jedem Tag die Taste Enter.";
+$locale['album_0007'] = "Sichtbarkeit";
+$locale['album_0008'] = "Sprache";
+$locale['album_0009'] = "Titelbild";
+$locale['album_0010'] = "Maximale Dateigröße: %s / Erlaubte Dateitypen: %s / Maximale Abmessungen: %s Pixel x %s Pixel";
+$locale['album_0011'] = "Position";
+$locale['album_0012'] = "Speichern";
+$locale['album_0013'] = "Das Fotoalbum wurde erfolgreich aktualisiert.";
+$locale['album_0014'] = "Das Fotoalbum wurde erfolgreich erstellt.";
+$locale['album_0015'] = "Du musst einen Titel angeben.";
+$locale['album_0016'] = "Titelbild des Fotoalbums entfernen";
+
+// Gallery actions & front page (first tab)
+$locale['album_0020'] = "Optionen";
+$locale['album_0021'] = "Aufwärts";
+$locale['album_0022'] = "Abwärts";
+$locale['album_0023'] = "Entfernen";
+$locale['album_0024'] = "Bearbeiten";
+$locale['album_0025'] = "Das Album wurde erfolgreich aufwärts verschoben.";
+$locale['album_0026'] = "Das Album wurde erfolgreich abwärts verschoben.";
+$locale['album_0027'] = "Fotos vorhanden";
+$locale['album_0028'] = "Komplettes Album inklusive Fotos entfernen";
+$locale['album_0029'] = "Enthaltene Fotos nach %s verschieben";
+$locale['album_0030'] = "Das Album wurde erfolgreich entfernt.";
+$locale['album_0031'] = "Alle enthaltenen Fotos wurden nach %s verschoben.";
+$locale['album_0032'] = "%d Foto(s) wurde/wurden aus dem Album entfernt.";
+
+// Photo Form
+$locale['photo_0001'] = "Titel";
+$locale['photo_0002'] = "Titel des Fotos";
+$locale['photo_0003'] = "Album";
+$locale['photo_0004'] = "Foto";
+$locale['photo_0005'] = "Tags";
+$locale['photo_0006'] = $locale['album_0005']; // i put like this, and no dependencies.
+$locale['photo_0007'] = $locale['album_0006'];
+$locale['photo_0008'] = "Beschreibung";
+$locale['photo_0009'] = "Beschreibung des Fotos";
+$locale['photo_0010'] = "Kommentare zu diesem Foto aktivieren";
+$locale['photo_0011'] = "Bewertungen zu diesem Foto aktivieren";
+$locale['photo_0012'] = "Speichern";
+$locale['photo_0013'] = "Position";
+$locale['photo_0014'] = "Du musst ein Foto hochladen.";
+$locale['photo_0015'] = "Das Foto wurde erfolgreich aktualisiert.";
+$locale['photo_0016'] = "Das Foto wurde erfolgreich hinzugefügt.";
+$locale['photo_0017'] = $locale['album_0010'];
+$locale['photo_0018'] = "Entfernen";
+$locale['photo_0019'] = "Du kannst insgesamt 20 Fotos gleichzeitig hochladen.<br />\n";
+$locale['photo_0019'] .= "Klicke auf \"Fotos hinzufügen\" und anschließend halte die Taste \"STRG\" und klicke die Fotos an, welche du hochladen möchtest.<br />";
+$locale['photo_0019'] .= "Anschließend klicke auf \"Fotos hochladen\" um die Fotos dem Album hinzuzufügen.";
+$locale['photo_0020'] = "Fotos hochladen";
+$locale['photo_0021'] = "Es wurde/wurden %d Foto/Fotos hinzugefügt.";
+$locale['photo_0021a'] = "Es wurde/wurden %d Fotos aufgrund eines Fehlers nicht hochgeladen.";
+$locale['photo_0022'] = "Das Foto wurde erfolgreich aufwärts verschoben.";
+$locale['photo_0023'] = "Das Foto wurde erfolgreich abwärts verschoben.";
+$locale['photo_0024'] = "Das Foto wurde erfolgreich entfernt.";
+$locale['photo_0025'] = "Alle Fotos entfernen";
+$locale['photo_0026'] = "<strong>Achtung!</strong> Es werden <strong>alle Fotos unwiederruflich</strong> aus dem Album <strong>entfernt</strong>. Bist du dir sicher?";
+$locale['photo_0027'] = "Fotos entfernen";
+$locale['photo_0028'] = "Abbrechen";
+
+// Submissions form
+$locale['gallery_0100'] = "Einsendungen";
+$locale['gallery_0101'] = "Vielen Dank für die Einsendung des Fotos.";
+$locale['gallery_0102'] = "Weiteres Foto einsenden";
+$locale['gallery_0103'] = $locale['photo_0003'];
+$locale['gallery_0104'] = $locale['photo_0001'];
+$locale['gallery_0105'] = $locale['photo_0005'];
+$locale['gallery_0106'] = $locale['album_0006'];
+$locale['gallery_0107'] = "Du kannst das nachfolgende Formular für die Einsendung eines neuen Fotos benützen. ";
+$locale['gallery_0107'] .= "Deine Einsendung wird anschließend durch einen Administrator überprüft.<br />";
+$locale['gallery_0107'] .= fusion_get_settings("sitename")." behält sich das Recht vor, das Foto zu erweitern, abzuändern oder gegebenenfalls zu entfernen. ";
+$locale['gallery_0107'] .= "Bitte beachte, dass der Inhalt des Fotos passend zu dem Thema der Seite ist.";
+$locale['gallery_0106'] = $locale['photo_0008'];
+$locale['gallery_0109'] = $locale['photo_0004'];
+$locale['gallery_0110'] = $locale['photo_0014'];
+$locale['gallery_0111'] = "Foto einsenden";
+$locale['gallery_0112'] = "Entschuldigung! Wir akzeptieren derzeit keine neuen Einsendungen von Fotos.";
+$locale['gallery_0113'] = "Zurück zur Startseite";
+
+// Submissions admin
+$locale['gallery_0150'] = "Es sind derzeit keine neuen Fotos eingesendet worden.";
+$locale['gallery_0151'] = "Es ist/sind derzeit %s verfügbar, welche auf eine Überprüfung warten.";
+$locale['gallery_0152'] = "Titel";
+$locale['gallery_0153'] = "Einsender";
+$locale['gallery_0154'] = "Datum";
+$locale['gallery_0155'] = "ID";
+$locale['gallery_0156'] = "Das nachfolgende Foto wurde eingesendet von ";
+$locale['gallery_0157'] = "Die Einsendung erfolgte ";
+$locale['gallery_0158'] = "Veröffentlichen";
+$locale['gallery_0159'] = "Entfernen";
+$locale['gallery_0160'] = "Das Foto wurde erfolgreich veröffentlicht.";
+$locale['gallery_0161'] = "Das Foto wurde erfolgreich entfernt.";
+
+// Settings
+$locale['gallery_0200'] = "Foto Einsendungen erlauben";
+$locale['gallery_0201'] = "Foto Beschreibung verpflichtend";
+$locale['gallery_0202'] = "Fotos pro Seite";
+$locale['gallery_0203'] = "Größe der Vorschaufotos";
+$locale['gallery_0204'] = "Breite x Höhe";
+$locale['gallery_0205'] = "Größe des Fotos";
+$locale['gallery_0206'] = "Maximale Größe des Fotos";
+$locale['gallery_0207'] = "Maximale Dateigröße des Fotos";
+$locale['gallery_0208'] = "Farbe des Wasserzeichens (Albentitel)";
+$locale['gallery_0209'] = "Farbe des Wasserzeichens (Albenbeschreibung)";
+$locale['gallery_0210'] = "Farbe des Wasserzeichens (Fototitel)";
+$locale['gallery_0211'] = "Existierende Wasserzeichen entfernen";
+$locale['gallery_0212'] = "Dateipfad zu einem eigenen .png Wasserzeichen";
+$locale['gallery_0213'] =  "Text Wasserzeichen auf Fotos verwenden";
+$locale['gallery_0214'] = "Wasserzeichen für Fotos";
+$locale['gallery_0215'] = "Erstellte Wasserzeichen speichern";
+$locale['gallery_0216'] = "Einstellungen speichern";
+
+// temporary
+// Error Album messages
+$locale['635'] = "Album";
+$locale['636'] = "Hochgeladen von";
+$locale['637'] = "Veröffentlichungsdatum";
+$locale['638'] = "Bewertung";
+$locale['639'] = "Kommentar";
+$locale['640'] = "Beschreibung";
+$locale['641'] = "Angesehen";
+$locale['642'] = "Bewertungen";
+$locale['643'] = "Kommentare";
+$locale['644'] = "Abmessungen";
+$locale['645'] = "Dateityp";
+$locale['646'] = "Kanäle";
+$locale['647'] = "Bits";
+$locale['648'] = "ISO";
+$locale['649'] = "Belichtung";
+$locale['650'] = "Blende";
+$locale['651'] = "Kamera";
+$locale['652'] = "Kamera Model";
+$locale['655'] = "Tags: ";
+$locale['660'] = "Es wurden bisher noch keine Fotos diesen Album hinzugefügt.";
+$locale['702'] = "Das Foto wurde erfolgreich hinzugefügt.";
+$locale['703'] = "Das Foto wurde erfolgreich aktualisiert.";
+
+// Photo Gallery Settings
+$locale['600'] = "Galerie";
+$locale['601'] = "Größe des Vorschaufotos";
+$locale['602'] = "Größe des Fotos";
+$locale['603'] = "Maximale Größe des Fotos";
+$locale['604'] = "Breite x Höhe";
+$locale['605'] = "Maximale Dateigröße des Fotos";
+$locale['606'] = "Thumb-Komprimierungsmethode";
+$locale['607'] = "GD1";
+$locale['608'] = "GD2";
+$locale['609'] = "Fotos pro Reihe (Fronted)";
+$locale['609b'] = "Fotos pro Reihe (Backend)";
+$locale['610'] = "Fotos pro Seite";
+$locale['611'] = "Wasserzeichen für Fotos?";
+$locale['612'] = "Dateipfad zu einem eigenen .png Wasserzeichen";
+$locale['613'] = "Text Wasserzeichen auf Fotos verwenden";
+$locale['614'] = "Farbe des Wasserzeichens (Albentitel)";
+$locale['615'] = "Farbe des Wasserzeichens (Albenbeschreibung)";
+$locale['616'] = "Farbe des Wasserzeichens (Fototitel)";
+$locale['617'] = "Erstellte Wasserzeichen speichern";
+$locale['618'] = "Reduziert Serverladezeiten, benötigt mehr Server Speicherplatz.";
+$locale['619'] = "Existierende Wasserzeichen entfernen";
+$locale['620'] = "Existierende Wasserzeichen entfernen";
+$locale['621'] = "Bild hochladen";
+
+?>
