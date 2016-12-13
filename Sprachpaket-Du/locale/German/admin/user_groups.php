ec2c45831cee0b9bd966a4afb5df818f87aa851d
David <david_guetl@gmx.at>
2016-12-13 09:17:58 +0100

    Änderungen admin/user_groups.php (PHP-Fusion UK)

--- Sprachpaket-Du/locale/German/admin/user_groups.php
+++ Sprachpaket-Du/locale/German/admin/user_groups.php
@@ -3,50 +3,59 @@
 // Translate powered by PHPFusion-Development.at
 
 // Edit/Save/Delete User Group Messages
-$locale['400'] = "Die Benutzergruppe wurde erfolgreich aktualisiert.";
-$locale['401'] = "Die Benutzergruppe wurde erfolgreich erstellt.";
-$locale['402'] = "Die ausgewählten Benutzer wurden aus der Benutzergruppe erfolgreich entfernt.";
-$locale['403'] = "Alle Benutzer wurden aus der Benutzergruppe erfolgreich entfernt.";
-$locale['404'] = "Die ausgewählten Benutzer wurden der Benutzergruppe erfolgreich hinzugefügt.";
-$locale['405'] = "Die Benutzergruppe kann nicht entfernt werden.";
-$locale['406'] = "Es befinden sich noch Benutzer in der Benutzergruppe.";
-$locale['407'] = "Die Benutzergruppe wurde erfolgreich entfernt.";
+$locale['GRP_400'] = "Die Benutzergruppe wurde erfolgreich aktualisiert.";
+$locale['GRP_401'] = "Die Benutzergruppe wurde erfolgreich erstellt.";
+$locale['GRP_403'] = "Es wurden keine Benutzer ausgewählt.";
+$locale['GRP_404'] = "Es wurde keine Benutzergruppe ausgewählt."; //??
+$locale['GRP_405'] = "Die Benutzergruppe kann nicht entfernt werden.";
+$locale['GRP_406'] = "Es befinden sich noch Benutzer in der Benutzergruppe.";
+$locale['GRP_407'] = "Die Benutzergruppe wurde erfolgreich entfernt.";
+$locale['GRP_408'] = "Es wurde kein Benutzer oder keine Benutzergruppe gewählt.";
+$locale['GRP_409'] = " Erstelle eine Benutzrgruppe.";
+$locale['GRP_410'] = " Ein Benutzer der Benutzergruppe.";
+$locale['GRP_411'] = " Der Benutzer wurde aus der Benutzergruppe entfernt.";
+$locale['GRP_412'] = " Der Benutzer ist kein Mitglied der Benutzergruppe.";
 
 // Edit/Delete Options
-$locale['420'] = "Benutzergruppen";
-$locale['421'] = "Bearbeiten";
-$locale['422'] = "Entfernen";
-$locale['423'] = "Bist du dir sicher, dass du die Benutzergruppe entfernen möchtest?";
+$locale['GRP_420'] = "Benutzergruppen";
+$locale['GRP_421'] = "Benutzergruppe bearbeiten";
+$locale['GRP_422'] = "Benutzergruppe entfernen";
+$locale['GRP_423'] = "Mitglieder Verwaltung";
+$locale['GRP_424'] = "Es wird / werden %d Benutzergruppen angezeigt.";
+$locale['GRP_425'] = "Bist du dir sicher, dass du die Benutzergruppe entfernen möchtest?";
+$locale['GRP_426'] = "Standardgruppe";
+$locale['GRP_427'] = "Es wird / werden derzeit %d von %d Mitglied / Mitglieder angezeigt.";
+$locale['GRP_428'] = "Benutzergruppe erstellen";
 
 // Edit/Add User Group
-$locale['430'] = "Benutzergruppe bearbeiten";
-$locale['431'] = "Benutzergruppe erstellen";
-$locale['432'] = "Name der Benutzergruppe";
-$locale['433'] = "Beschreibung";
-$locale['434'] = "Speichern";
+$locale['GRP_432'] = "Name";
+$locale['GRP_433'] = "Beschreibung";
+$locale['GRP_434'] = "Speichern";
+$locale['GRP_435'] = "ID";
+$locale['GRP_436'] = "Icon";
+$locale['GRP_437'] = "Optionen";
+$locale['GRP_438'] = "Benutzer";
+$locale['GRP_439'] = "Icon";
 
 // Search/Add Group Users
-$locale['440'] = "Benutzer hinzufügen";
-$locale['441'] = "Um Benutzer in diese Benutzergruppe hinzuzufügen, benütze bitte das Formular unterhalb.";
-$locale['442'] = "Du kannst nach mehreren Benutzern suchen, wenn du die einzelnen Suchbegriffe mit einem Komma trennst.";
-$locale['443'] = "Benutzer IDs";
-$locale['444'] = "Benutzernamen";
-$locale['445'] = "Suche starten";
-$locale['446'] = "Benutzername";
-$locale['447'] = "Benutzerlevel";
-$locale['448'] = "Alle markieren";
-$locale['449'] = "Alle demarkieren";
-$locale['450'] = "Markierte Benutzer hinzufügen";
-$locale['451'] = "Es wurden keine Benutzer gefunden.";
-$locale['452'] = "Suche erneut starten";
+$locale['GRP_440'] = "Benutzer hinzufügen";
+$locale['GRP_441'] = "Um Benutzer in diese Benutzergruppe hinzuzufügen, benütze bitte das Formular unterhalb.";
+$locale['GRP_442'] = "Du kannst nach mehreren Benutzern suchen, wenn du die einzelnen Suchbegriffe mit einem Komma trennst.";
+$locale['GRP_446'] = "Benutzername";
+$locale['GRP_447'] = "Benutzerlevel";
+$locale['GRP_448'] = "Alle markieren";
+$locale['GRP_449'] = "Alle demarkieren";
+$locale['GRP_450'] = "Markierte Benutzer hinzufügen";
+$locale['GRP_451'] = "Benutzer suchen";
+$locale['GRP_452'] = "Aktuelle Benutzergruppe";
 
 // Current Group Users
-$locale['460'] = "Vorhandene Benutzer";
-$locale['461'] = "Ausgewählte Benutzer entfernen";
-$locale['462'] = "Alle Benutzer entfernen";
-$locale['463'] = "Es befinden sich derzeit keine Benutzer in dieser Benutzergruppe.";
+$locale['GRP_460'] = "Vorhandene Benutzer";
+$locale['GRP_461'] = "Ausgewählte Benutzer entfernen";
+$locale['GRP_462'] = "Alle Benutzer entfernen";
+$locale['GRP_463'] = "Es befinden sich derzeit keine Benutzer in dieser Benutzergruppe.";
 
 // Error Messages
-$locale['464'] = "Du musst einen Namen für die Benutzergruppe angeben.";
+$locale['GRP_464'] = "Du musst einen Namen für die Benutzergruppe angeben.";
 
 ?>
