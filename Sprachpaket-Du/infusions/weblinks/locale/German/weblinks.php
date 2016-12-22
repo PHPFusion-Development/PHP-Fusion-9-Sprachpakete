eac15df6b59c4e218a9a26d49d81fdb1d4dad194
David <david_guetl@gmx.at>
2016-12-22 06:39:19 +0100

    Addon "Weblinks"

---
+++ Sprachpaket-Du/infusions/weblinks/locale/German/weblinks.php
@@ -0,0 +1,12 @@
+<?php
+
+// Translate powered by PHPFusion-Development.at
+
+$locale['400'] = "Weblinks";
+$locale['410'] = "NEU";
+$locale['411'] = "Veröffentlichungsdatum";
+$locale['412'] = "Klicks";
+
+// Hinweise
+$locale['430'] = "Es wurden bisher noch keine Weblink Kategorien erstellt.";
+$locale['431'] = "Es wurden bisher noch keine Weblinks in dieser Kategorie abgelegt.";
---
+++ Sprachpaket-Du/infusions/weblinks/locale/German/weblinks_admin.php
@@ -0,0 +1,119 @@
+<?php
+
+// Translate powered by PHPFusion-Development.at
+
+// Web Link Category titles
+$locale['402'] = "Current Web Link Categories";
+
+// Web Link titles
+$locale['wl_0001'] = "Weblink erstellen";
+$locale['wl_0002'] = "Weblink bearbeiten";
+$locale['wl_0003'] = "Alle Weblink Einträge";
+$locale['wl_0004'] = "Weblink Kategorien";
+$locale['wl_0005'] = "Weblink Kategorie bearbeiten";
+$locale['wl_0006'] = "Allgemeine Einstellungen für das Weblinks System.";
+$locale['wl_0007'] = "Es ist/sind derzeit %s verfügbar, welche auf eine Überprüfung wartet/warten.";
+$locale['wl_0008'] = "Es sind derzeit keine neuen Weblinks eingesendet worden.";
+
+// Web Link form
+$locale['wl_0100'] = "Titel";
+$locale['wl_0101'] = "Seitenname";
+$locale['wl_0102'] = "Du musst einen Seitennamen angeben.";
+$locale['wl_0103'] = "Beschreibung";
+$locale['wl_0104'] = "Adresse";
+$locale['wl_0105'] = "Kategorie";
+$locale['wl_0106'] = "Sichtbarkeit";
+$locale['wl_0107'] = "Das Veröffentlichungsdatum nach dem Speichern erneuern.";
+$locale['wl_0108'] = "Speichern";
+
+// Current Web Links
+$locale['wl_0200'] = "Weblinks";
+$locale['wl_0201'] = "Kategorie";
+$locale['wl_0203'] = "Adresse";
+$locale['wl_0204'] = "ID";
+$locale['wl_0205'] = "Bearbeiten";
+$locale['wl_0206'] = "Entfernen";
+$locale['wl_0207'] = "Es wurden bisher noch keine Weblinks erstellt.";
+$locale['wl_0208'] = "Optionen";
+
+// Web Link messages
+$locale['wl_0300'] = "Der Weblink wurde erfolgreich erstellt.";
+$locale['wl_0301'] = "Der Weblink wurde erfolgreich aktualisiert.";
+$locale['wl_0302'] = "Der Weblink wurde erfolgreich entfernt.";
+$locale['wl_0303'] = "Bist du dir sicher, dass du diesen Weblink entfernen möchtest?";
+
+// Web Link Category messages
+$locale['wl_0304'] = "Die Weblink Kategorie wurde erfolgreich erstellt.";
+$locale['wl_0305'] = "Die Weblink Kategorie wurde erfolgreich aktualisiert.";
+$locale['wl_0306'] = "Die Weblink Kategorie wurde erfolgreich entfernt.";
+$locale['wl_0307'] = "Die Weblink Kategorie konnte nicht entfernt werden.";
+$locale['wl_0308'] = "Es befinden sich noch Unterkategorien oder Einträge in der Kategorie.";
+$locale['wl_0309'] = "Es existiert bereits eine gleichnamige Kategorie.";
+$locale['wl_0310'] = "Bist du dir sicher, dass du diese Weblink Kategorie entfernen möchtest?";
+
+// Filters
+$locale['wl_0400'] = "Sortierung";
+$locale['wl_0401'] = "Filter show category by";
+$locale['wl_0402'] = "Alle Einträge";
+
+// Add/Edit Web Link Category Form
+$locale['wl_0700'] = "Titel";
+$locale['wl_0701'] = "Du musst einen Titel angeben.";
+$locale['wl_0702'] = "Beschreibung";
+$locale['wl_0703'] = "Als Hauptkategorie";
+$locale['wl_0704'] = "Sortierung";
+$locale['wl_0705'] = "Weblink ID";
+$locale['wl_0706'] = "Weblink Titel";
+$locale['wl_0707'] = "Weblink Datum";
+$locale['wl_0708'] = "Aufwärts (A-Z)";
+$locale['wl_0709'] = "Abwärts (Z-A)";
+$locale['wl_0710'] = "Kategorien Editor";
+$locale['wl_0711'] = "Speichern";
+$locale['536'] = "Es wurden bisher noch keine Weblink Kategorien erstellt.";
+$locale['537'] = "Es wurden bisher noch keine Weblink Kategorien erstellt.";
+$locale['538'] = "Du musst mindestens eine Weblink Kategorie erstellen, bevor du Weblinks erstellen kannst.";
+$locale['539'] = "Klicke Hier, ";
+$locale['540'] = " um die Weblink Kategorien aufzurufen.";
+
+// Submissions
+$locale['wl_0500'] = "Einsendungen";
+$locale['wl_0501'] = "Derzeit werden %d Weblinks von %d Weblinks angezeigt.";
+$locale['wl_0503'] = "Titel";
+$locale['wl_0504'] = "Einsender";
+$locale['wl_0505'] = "Datum";
+$locale['wl_0506'] = "ID";
+$locale['wl_0507'] = "Die Einsendung wurde erfolgreich entfernt.";
+$locale['wl_0508'] = "Veröffentlichen";
+$locale['wl_0509'] = "Die Einsendung wurde erfolgreich veröffentlicht.";
+$locale['wl_0510'] = "Der nachfolgende Weblink wurde eingesendet von ";
+$locale['wl_0511'] = "Die Einsendung erfolgte ";
+$locale['wl_0512'] = "Entfernen";
+
+// Public
+$locale['wl_0800'] = "Weblink einsenden";
+
+// Submit Link Success
+$locale['wl_0801'] = "Vielen Dank für die Einsendung des Weblinks.";
+$locale['wl_0802'] = "Weiteren Weblink einsenden";
+$locale['wl_0803'] = "Zurück zur Startseite";
+
+// Submit Link Form
+$locale['wl_0804'] = "Du kannst das nachfolgende Formular für die Einsendung eines neuen Weblinks benützen.  ";
+$locale['wl_0804'] .= "Deine Einsendung wird anschließend durch einen Administrator überprüft.<br />";
+$locale['wl_0804'] .= "[SITENAME] behält sich das Recht vor, den Weblink zu erweitern, abzuändern oder gegebenenfalls zu entfernen. ";
+$locale['wl_0804'] .= "Bitte beachte, dass der Inhalt des Weblinks passend zu dem Thema der Seite ist.";
+$locale['wl_0805'] = "Kategorie";
+$locale['wl_0806'] = "Titel";
+$locale['wl_0807'] = "Adresse";
+$locale['wl_0808'] = "Beschreibung";
+$locale['wl_0809'] = "Entschuldigung! Wir akzeptieren derzeit keine neuen Einsendungen von Weblinks.";
+
+// Settings
+$locale['wl_0600'] = "Einstellungen";
+$locale['wl_0601'] = "Weblink Einsendungen erlauben?";
+$locale['wl_0602'] = "Beschreibung verpflichtend?";
+$locale['601'] = "Die Einstellungen wurden erfolgreich aktualisiert.";
+$locale['602'] = "Weblinks";
+$locale['wl_603'] = "Einträge pro Seite";
+$locale['wl_604'] = "Speichern";
+$locale['error_value'] = "Du musst einen Wert angeben.";
