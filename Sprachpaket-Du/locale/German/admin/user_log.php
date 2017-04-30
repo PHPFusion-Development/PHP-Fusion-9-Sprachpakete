b3d1f7b9ac4001b3906397fa85243f8bb72e2224
David <david_guetl@gmx.at>
2017-04-30 05:26:38 +0200

    Aktualisierung für PHP-Fusion 9 Final

--- Sprachpaket-Du/locale/German/admin/user_log.php
+++ Sprachpaket-Du/locale/German/admin/user_log.php
@@ -3,26 +3,24 @@
 // Translate powered by PHPFusion-Development.at
 
 // Main
-$locale['100'] = "Benutzerlog";
-$locale['101'] = "ID";
-$locale['102'] = "Datum";
-$locale['103'] = "Benutzername";
-$locale['103a'] = "E-Mail Adresse";
-$locale['104'] = "Benutzerfeld";
-$locale['105'] = "Alter Wert";
-$locale['106'] = "Neuer Wert";
-$locale['107'] = "Sortierung";
-$locale['108'] = "Benutzer ID / Benutzername";
-$locale['109'] = "Sortierung anwenden";
-$locale['110'] = "Log Aufzeichnungen entfernen, welche älter sind als";
-$locale['111'] = "Tage";
-$locale['112'] = "Es ist derzeit keine Log Aufzeichnung vorhanden.";
-$locale['113'] = "Ausgewählter Benutzername";
-$locale['114'] = "Abbrechen";
-$locale['115'] = "Benutzerfeld";
-$locale['116'] = "Entfernen";
-$locale['117'] = "Optionen";
-$locale['118'] = "Alle Log Aufzeichnen welche älter als %s Tage waren wurden erfolgreich entfernt.";
-$locale['119'] = "Die Log Aufzeichnung wurde erfolgreich entfernt.";
+$locale['UL_001'] = "Benutzerlog";
+$locale['UL_002'] = "Datum";
+$locale['UL_003'] = "Benutzername";
+$locale['UL_004'] = "Benutzerfeld";
+$locale['UL_005'] = "Alle Log Aufzeichnen welche älter als %d Tag(e) waren wurden erfolgreich entfernt.";
+$locale['UL_006'] = "Die Log Aufzeichnung wurde erfolgreich entfernt.";
+$locale['UL_007'] = "E-Mail Adresse";
+$locale['UL_008'] = "Sortierung";
+$locale['UL_009'] = "Benutzer ID / Benutzername";
+$locale['UL_010'] = "Benutzerfeld";
+$locale['UL_011'] = "Entfernen";
+$locale['UL_012'] = "Alter Wert";
+$locale['UL_013'] = "Neuer Wert";
+$locale['UL_014'] = "Optionen";
+$locale['UL_015'] = "Es ist derzeit keine Log Aufzeichnung vorhanden.";
+$locale['UL_016'] = "Log Aufzeichnungen entfernen, welche älter sind als";
+$locale['UL_017'] = "Tage";
+$locale['UL_018'] = "Aufwärts (A-Z)";
+$locale['UL_019'] = "Abwärts (Z-A)";
 
 ?>
