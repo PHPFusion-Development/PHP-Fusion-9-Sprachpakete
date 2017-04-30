532fc4338e82102a6db1525f14a45961e40961e4
David <david_guetl@gmx.at>
2017-04-30 09:01:35 +0200

    Aktualiserungen für PHP-Fusion 9 Stable

--- Sprachpaket-Du/locale/German/admin/image_uploads.php
+++ Sprachpaket-Du/locale/German/admin/image_uploads.php
@@ -3,7 +3,7 @@
 // Translate powered by PHPFusion-Development.at
 
 // Delete Image
-$locale['400'] = "Das Bild wurde erfolgreich entfernt.";
+$locale['400'] = "Bild entfernen";
 $locale['401'] = "Das Bild wurde erfolgreich entfernt.";
 $locale['402'] = "Zurück zur Bilder Verwaltung";
 $locale['403'] = "Zurück zum Dashboard";
@@ -16,9 +16,9 @@ $locale['423'] = "Artikel";
 $locale['424'] = "News";
 $locale['425'] = "Es können nur Bilder im gif, jpg oder png Dateiformat hochgeladen werden.";
 $locale['426'] = "Das Bild wurde erfolgreich hochgeladen.";
-$locale['427'] = "News Kategorien";
+$locale['427'] = "Newskategorien";
 $locale['428'] = "Blog";
-$locale['429'] = "Blog Kategorien";
+$locale['429'] = "Blogkategorien";
 
 // View Image
 $locale['440'] = "Bild ansehen";
@@ -33,6 +33,4 @@ $locale['463'] = "Es wurden für diesen Bereich noch keine Bilder hochgeladen.";
 $locale['464'] = "Bilder für den TinyMCE Editor aktualisieren";
 
 // Popup Error/Warning Messages
-$locale['470'] = "Bist du dir sicher, dass du das Bild entfernen möchtest?";
- 
-?>
+$locale['470'] = "Bist du dir sicher, dass du dieses Bild entfernen möchtest?";
